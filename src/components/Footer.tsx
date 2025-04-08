1a1bdb9a9b0b74f24e1ce9e0ff198c8e09b618f8
McJuma <164210655+McJuma@users.noreply.github.com>
2025-04-08 10:24:28 +0000

    Style "Donate Now" link in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
               TashaSashaFoundation is a non-profit organization dedicated to improving lives through education, healthcare, and community development programs.
             </p>
             <p>
-              <a href="#">Donate Now</a>
+              <a href="#" className="bg-blue-500 rounded-md hover:bg-white text-white px-4 py-2">Donate Now</a>
             </p>
           </div>
 
